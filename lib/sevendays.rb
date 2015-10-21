78fda461482c3730ef9f0c17fc2ae81ffbb8fc4e
ericathegreat <EricaTheGreat@gmail.com>
2015-10-22 08:33:46 +1100

    Now with basic navigation

--- lib/sevendays.rb
+++ lib/sevendays.rb
@@ -1,5 +1,21 @@
-require "sevendays/version"
+require 'highline/import'
+require_relative "sevendays/version"
+require_relative "sevendays/world"
+require_relative "sevendays/player"
 
 module Sevendays
-  # Your code goes here...
+	world = World.new.world
+	player = Player.new world
+
+	puts player.report
+
+	choose do |menu|
+		menu.prompt = "Options:"
+		world.interactions(player).each do |interaction|
+			menu.choice(interaction.display_text) {interaction.execute}
+		end
+	end
+	
+	puts player.report
+
 end
