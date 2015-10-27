78155852f60f596f6cf801924a634323bb828a83
ericathegreat <EricaTheGreat@gmail.com>
2015-10-28 08:52:16 +1100

    Tidy

--- lib/sevendays/interactable.rb
+++ lib/sevendays/interactable.rb
@@ -1,12 +1,14 @@
 module Interactable
 	def has_interaction interaction
-		@all_interactions ||= []
-		@all_interactions << interaction
+		all_interactions << interaction
 	end
 
 	def interactions player
+		all_interactions.select { |interaction| interaction.valid? player }
+	end
+
+	def all_interactions
 		@all_interactions ||= []
-		@all_interactions.select { |interaction| interaction.valid? player }
 	end
 
 end
