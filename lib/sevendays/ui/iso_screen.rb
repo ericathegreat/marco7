096a1c6e04f4e2ea90133da7f639b30296a5f6d8
ericathegreat <EricaTheGreat@gmail.com>
2015-12-21 17:43:25 +1100

    Only draw tiles which are in range of the screen

--- lib/sevendays/ui/iso_screen.rb
+++ lib/sevendays/ui/iso_screen.rb
@@ -14,10 +14,16 @@ module UI
 			@player = Registry.instance.sprite(:player)
 		end
 
-		def draw #132 pixels wide
-			(-1..20).each do |r|
-				(-1..20).each do |c|
-					draw_tile r,c
+		def draw
+			r_world_top_left, c_world_top_left = screen_to_world(0, 0)
+
+			@draw_columns ||= (GameWindow::WINDOW_WIDE/COLUMN_WIDTH/2 + 1).ceil
+			@draw_rows ||= (GameWindow::WINDOW_HIGH/ROW_HEIGHT/2 + 1).ceil
+
+			(-1..@draw_columns).each do |r|
+				(-1..@draw_rows).each do |c|
+					draw_tile (r_world_top_left.floor + r + c), (c_world_top_left.floor + c - r)
+					draw_tile (r_world_top_left.floor + r + c + 1), (c_world_top_left.floor + c - r)
 				end
 			end
 			@player.draw( *world_to_screen(*Player.instance.player_world_space, 1) )
@@ -31,6 +37,7 @@ module UI
 		def render_center
 			return world_to_render(*Player.instance.player_world_space)
 		end
+
 
 		def world_to_render r_world, c_world
 			r_px_render = (r_world + c_world) * ROW_HEIGHT
@@ -52,9 +59,9 @@ module UI
 		end
 
 		def screen_to_world r_px_screen, c_px_screen
-			r_origin, c_origin = render_top_left
-			c_px_render = c_px_screen + c_origin - GameWindow::WINDOW_WIDE/2
-			r_px_render = r_px_screen + r_origin - GameWindow::WINDOW_HIGH/2
+			r_origin, c_origin = render_center
+			c_px_render = c_origin + c_px_screen - GameWindow::WINDOW_WIDE/2
+			r_px_render = r_origin + r_px_screen - GameWindow::WINDOW_HIGH/2
 
 			r = ((c_px_render / COLUMN_WIDTH) + (r_px_render / ROW_HEIGHT)) / 2
 			c = r_px_render / ROW_HEIGHT - r
@@ -76,7 +83,7 @@ module UI
 		end
 
 		def click x, y
-			# puts screen_to_world(y, x).map{|a| a.round}.inspect
+			puts screen_to_world(y, x).map{|a| a.round}.inspect
 			#r_origin, c_origin = Player.instance.player_world_space
 			#Player.instance.player_world_space= [y - (GameWindow::WINDOW_WIDE/2) + r_origin, x - ( GameWindow::WINDOW_HIGH/2) + c_origin]
 		end
