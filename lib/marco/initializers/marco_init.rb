7b25966429d67a2a0918cb49a07e660790e297ad
ericathegreat <EricaTheGreat@gmail.com>
2016-05-23 19:06:29 +1000

    Some more marco animations

--- lib/marco/initializers/marco_init.rb
+++ lib/marco/initializers/marco_init.rb
@@ -5,10 +5,11 @@ require_relative "../ui/sprites/animation_group"
 class TilesInitializer < Initializer
 
 	register do |r|
-		puts "Initializing Marco"
+		puts "Initializing Marco art"
 
-		marco_images = Gosu::Image.load_tiles("img/marco-walk.png", 118, 100, tileable: true)
-		puts "MARCO #{marco_images.size}"
+		# marco_images = Gosu::Image.load_tiles("img/marco-walk.png", 118, 100, tileable: true)
+		marco_images = Gosu::Image.load_tiles("img/marco-idle-snapjaw-000.png", 118, 100, tileable: true)
+		# marco_images = Gosu::Image.load_tiles("img/marco-idle-lookaround-000.png", 118, 100, tileable: true)
 		marco_sprites = marco_images.map do |image|
 			UI::Sprites::BaseSprite.new(image, 64, 100)
 		end
