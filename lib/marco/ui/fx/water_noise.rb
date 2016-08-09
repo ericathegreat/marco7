44b6d9c25a289cf841aae6a5302312b3f2d8efb8
ericathegreat <EricaTheGreat@gmail.com>
2016-08-09 18:25:45 +1000

    Don't need to send through window size every frame

--- lib/marco/ui/fx/water_noise.rb
+++ lib/marco/ui/fx/water_noise.rb
@@ -12,25 +12,26 @@ module UI
 uniform sampler2D in_Texture;
 varying vec2 var_TexCoord;
 uniform vec2 in_Topxy;
-uniform vec2 in_Windowsize;
+uniform vec2 in_WindowSize;
 
 void main()
 {
   gl_FragColor = texture2D(in_Texture, var_TexCoord);
 
   // Use [x, y] to create a 2D noise.
-  vec2 position1 = vec2(((var_TexCoord) * in_Windowsize + in_Topxy));
+  vec2 position1 = vec2(((var_TexCoord) * in_WindowSize + in_Topxy));
 
   // general blotchy backgrop
-  float brightness1 = snoise(position1 / 400.0) / 4.0 + 1.2;
+  float brightness1 = snoise(position1 / 400.0) / 4.0 + 1.0;
 	gl_FragColor.rgb *= brightness1;
 }
   END
 
 		  def self.shader topX, topY
-		    @noise ||= Ashton::Shader.new fragment: NOISE_FRAGMENT
+		    @noise ||= Ashton::Shader.new fragment: NOISE_FRAGMENT, uniforms: {
+	        window_size: [GameWindow::WINDOW_WIDE, GameWindow::WINDOW_HIGH]
+	      }
 		    @noise.topxy = [topY, topX]
-		    @noise.windowsize = [GameWindow::WINDOW_WIDE, GameWindow::WINDOW_HIGH]
 		    @noise
 		  end
 
